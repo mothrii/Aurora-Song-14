db3f4f2c06134aac4ed624a38238e4849b204944
BudaLope <65098677+BudaLope@users.noreply.github.com>
2026-07-17 21:49:48 -0400

    More!

--- Content.Shared/_AS/Power/PoweredComponentSystem.cs
+++ Content.Shared/_AS/Power/PoweredComponentSystem.cs
@@ -6,16 +6,16 @@ namespace Content.Shared.Power.Components;
 /// <summary>
 /// Handles <see cref="PoweredComponentComponent"/> component manipulation.
 /// </summary>
-public sealed class PoweredComponentSystem : EntitySystem
+public sealed partial class PoweredComponentSystem : EntitySystem
 {
-    [Dependency] private readonly SharedPowerReceiverSystem _receiver = default!;
+    [Dependency] private SharedPowerReceiverSystem _receiver = default!;
 
-public override void Initialize()
-{
-base.Initialize();
+    public override void Initialize()
+    {
+        base.Initialize();
 
-SubscribeLocalEvent<PoweredComponentComponent, PowerChangedEvent>(OnPowerChanged);
-}
+        SubscribeLocalEvent<PoweredComponentComponent, PowerChangedEvent>(OnPowerChanged);
+    }
     private void OnPowerChanged(Entity<PoweredComponentComponent> ent, ref PowerChangedEvent args)
     {
         if (args.Powered && !ent.Comp.Broken)
